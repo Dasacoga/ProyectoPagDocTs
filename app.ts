c89790a6b4dd6b965edff72aa376fced58637328
Santiago <santiagogarcia2711@gmail.com>
2023-07-03 14:41:09 -0500

    segundo commit

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 let pages = {
     'home' : `<h3>Bienvenido a Medpage </h3> En esta pagina podras ver a tus pacientes si eres doctor, y tomar tus citas si eres pasciente`,
-    'aboutus' : `<h3>Formulario para doctores</h3>
+    'doc' : `<h3>Formulario para Doctores</h3>
     <form class="form">
         Nombre: <input type="text" name="name" placeholder="Nombre" required> 
 
@@ -30,7 +30,7 @@ let pages = {
     </form>`,
 
     
-    'services' : `<h3>Formulario para Usuarios</h3>
+    'user' : `<h3>Formulario para Usuarios</h3>
     <form class="form">
         Nombre: <input type="text" name="name" placeholder="Nombre" required> 
 
@@ -49,6 +49,34 @@ let pages = {
         <div class="contact-form-response none">
         </div>
     </form>`,
+    'citas' : `<h3>Formulario para Citas</h3>
+    <form class="form">
+        Nombre: <input type="text" name="name" placeholder="Nombre" required> 
+
+        Apellido: <input type="text" name="apellido" placeholder="Apellido" required><br/><br/>
+
+        Especialidad:
+        <select name="Especialidad" id="lang">
+          <option value="Medicina general">Medicina general</option>
+          <option value="Cardiología">Cardiología</option>
+          <option value="Medicina interna">Medicina interna</option>
+          <option value="Dermatología">Dermatología</option>
+          <option value="Rehabilitación física">Rehabilitación física</option>
+          <option value="Psicología">Psicología</option>
+          <option value="Odontología">Odontología</option>
+          <option value="Radiología">Radiología</option>
+        </select>
+
+        Consultorio:
+        <input type="number" name="consultorio"  min="1" max="30" required><br/><br/>
+
+        Correo de contacto:
+        <input type="email" name="email" placeholder="email" required><br/><br/>
+
+        <input type="submit" value="Enviar">
+        <div class="contact-form-response none">
+        </div>
+    </form>`,
     'contact' : `Our Contact Us page<br/><br/><img src='media/arch.jpeg' />`
 };
 
@@ -58,11 +86,14 @@ function getPageContent(page:any){
         case 'home':
             contentToReturn = pages.home;
             break;
-        case 'aboutus':
-            contentToReturn = pages.aboutus;
+        case 'doc':
+            contentToReturn = pages.doc;
             break;
-        case 'services':
-            contentToReturn = pages.services;
+        case 'user':
+            contentToReturn = pages.user;
+            break;
+        case 'citas':
+            contentToReturn = pages.citas;
             break;
         case 'contact':
             contentToReturn = pages.contact;
